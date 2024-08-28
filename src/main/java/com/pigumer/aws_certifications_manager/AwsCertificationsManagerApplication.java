5da2671a0766e06501c6ab4655cb6d8ed6dd0034
Shigeki Shoji <takesection@gmail.com>
2024-08-28 14:46:37 +0900

    1st

---
+++ src/main/java/com/pigumer/aws_certifications_manager/AwsCertificationsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.pigumer.aws_certifications_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCertificationsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCertificationsManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pigumer/aws_certifications_manager/AwsCertificationsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pigumer.aws_certifications_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCertificationsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
